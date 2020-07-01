196f6a1ac69ddfed4c7fb65a4bf0c8bf2b909b8f
藤木幹也 <fujikimikiya@Mikibook.local>
2020-07-01 23:03:57 +0900

    2020-07-01

---
+++ chart1/practice02.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+text1 = "パトカー"
+text2 = "タクシー"
+
+ansText = ""
+
+for i in range(len(text1)):
+   ansText += text1[i]
+   ansText += text2[i]
+
+print(ansText)
---
+++ chart1/practice03.py
@@ -0,0 +1,1 @@
+text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
