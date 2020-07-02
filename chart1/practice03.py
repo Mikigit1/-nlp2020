a382b7a83d0b0fcb52a83059745b03c10d01b82b
藤木幹也 <fujikimikiya@Mikibook.local>
2020-07-02 21:54:40 +0900

    2020-07-02 コミット

--- chart1/practice03.py
+++ chart1/practice03.py
@@ -1,1 +1,24 @@
+# “Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.”という文を単語に分解し，
+# 各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
+
+import collections
+# charNum = collections.Counter(text)
+# print(charNum)
+
 text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+
+#単語の文字数カウント
+text = text.replace(",", "").replace(".", "")
+wordNum = [len(word) for word in text.split()]
+print(wordNum)
+
+
+#アルファベットの数カウント
+counter = {}
+for word in text:
+    if not word in counter:
+        counter[word] = 1
+    else:
+        counter[word] += 1
+
+print(counter)
