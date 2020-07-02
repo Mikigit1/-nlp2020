38fab2b21e3df0338eaf71775e38f03d7da94770
藤木幹也 <fujikimikiya@Mikibook.local>
2020-07-03 08:56:25 +0900

    2020-07-03　午前学習

--- chart1/practice04.py
+++ chart1/practice04.py
@@ -2,6 +2,17 @@
 # 1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字，それ以外の単語は先頭の2文字を取り出し，
 # 取り出した文字列から単語の位置（先頭から何番目の単語か）への連想配列（辞書型もしくはマップ型）を作成せよ．
 
-text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-text = text.replace(",","").replace(".","")
+text = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
+text = text.replace(",","").replace(".","").split()
 
+
+def wordTake(num , word):
+    if num in [1,5,6,7,8,9,15,16,19]:
+        return (word[0],num)
+    else:
+        return(word[:2],num)
+
+#enumerate関数の使用
+ans = [wordTake(num,word) for num , word in enumerate(text,1)]
+
+print(dict(ans))
