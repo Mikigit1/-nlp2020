ef60c6e434e1771eb01cb16e5c31cbb8ac2c3818
MikiyaFujiki <privmikiya0812@yahoo.co.jp>
2020-07-11 16:49:01 +0900

    2020-07-11 commit

--- chart2/practice02.py
+++ chart2/practice02.py
@@ -4,9 +4,13 @@
 '''
 
 '''
-sedコマンド：Stream Editor
+unixコマンドは
+sed -e 's/\t/ /g' chart2/popular-names.txt
+tr '\t' ' ' chart2/popular-names.txt
+expand -t 1 chart2/popular-names.txt
 '''
 
 with open('chart2/popular-names.txt','r') as file:
     for text in file:
+        # タブ(\t)を 空白へ置換（strip空白（文字の間は別）を除去）
         print(text.strip().replace("\t"," "))
