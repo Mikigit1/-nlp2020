9785d284071b9d4ec9d06d7ab0421d439e518f04
MikiyaFujiki <privmikiya0812@yahoo.co.jp>
2020-07-26 21:58:11 +0900

    2−17更新

--- chart2/practice17/practice17.py
+++ chart2/practice17/practice17.py
@@ -1,18 +1,8 @@
 #1列目の文字列の種類（異なる文字列の集合）を求めよ．確認にはcut, sort, uniqコマンドを用いよ．
+#https://note.nkmk.me/python-pandas-value-counts/
 
 import pandas as pd
 
 df = pd.read_table('chart2/popular-names.txt',header=None,names=['name','sex','number','year'])
-col1 = df['name']
 
-def count_word(col1):
-    counter = {}
-    for word in col1:
-        if not word in counter:
-            counter[word] = 1
-        else:
-            counter[word] += 1
-    return counter
-
-
-print(count_word(col1))
+print(df['name'].unique())
