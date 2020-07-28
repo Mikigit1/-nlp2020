41aca74731e75dafd33d6c8a4e9b505cfbf2fabf
MikiyaFujiki <mikiya.fujiki1@persol.co.jp>
2020-07-28 15:44:54 +0900

    2020-07-28 2-17 更新

--- chart2/practice17/practice17.py
+++ chart2/practice17/practice17.py
@@ -5,14 +5,4 @@ import pandas as pd
 df = pd.read_table('chart2/popular-names.txt',header=None,names=['name','sex','number','year'])
 col1 = df['name']
 
-def count_word(col1):
-    counter = {}
-    for word in col1:
-        if not word in counter:
-            counter[word] = 1
-        else:
-            counter[word] += 1
-    return counter
-
-
-print(count_word(col1))
+print(set(col1))
