1674bba1b4da2e158ebe38ff0a92deb36068bb28
MikiyaFujiki <mikiya.fujiki1@persol.co.jp>
2020-08-12 12:54:41 +0900

    practice20 更新

--- chart3/practice20.py
+++ chart3/practice20.py
@@ -2,6 +2,13 @@
 import gzip
 import json
 
+
+'''
+windows環境の場合、utf-8ではなく、cp932でコーディングされるため
+with gzip.open('chart3/jawiki-country.json.gz',mode='rt', encoding = 'utf-8_sig') as f:
+と記載する。
+'''
+
 with gzip.open('chart3/jawiki-country.json.gz',mode='rt') as f:
     for line in f:
         json_load = json.loads(line)
