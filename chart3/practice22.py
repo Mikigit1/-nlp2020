85e7e5a9563d87963c10cfe5cae5d145a977a702
MikiyaFujiki <privmikiya0812@yahoo.co.jp>
2020-08-18 21:39:43 +0900

    第3章22問目　完成

--- chart3/practice22.py
+++ chart3/practice22.py
@@ -1,3 +1,19 @@
+# 必要なモジュールのインポート
 import gzip
 import json
 import re
+
+def search():
+    with gzip.open('chart3/jawiki-country.json.gz',mode='rt',encoding='utf-8') as f:
+        for line in f:
+            json_load = json.loads(line)
+            if json_load['title'] == 'イギリス':
+                return json_load['text']
+
+#findall(pattern, string)	正規表現にマッチする部分文字列を全て探しだしリストとして返します。
+result = re.findall(r'^(.*\[\[Category:.*\]\].*)$',search(),re.MULTILINE)
+
+for line in result:
+    line = line.strip(r'^(.*\[\[Category:')
+    line = line.strip(r'.*\]\].*)$')
+    print(line.strip(r'\|.$'))
