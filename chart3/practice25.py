57af03f46057d77fa81f40431ff80fcf169f300b
MikiyaFujiki <privmikiya0812@yahoo.co.jp>
2020-09-14 22:18:27 +0900

    3-25　完了

--- chart3/practice25.py
+++ chart3/practice25.py
@@ -2,3 +2,18 @@
 25. テンプレートの抽出
 記事中に含まれる「基礎情報」テンプレートのフィールド名と値を抽出し，辞書オブジェクトとして格納せよ
 '''
+#<ref>
+import json
+import re 
+import gzip
+
+def search():
+    with gzip.open('chart3/jawiki-country.json.gz',mode='rt',encoding='utf-8') as f:
+        for line in f:
+            json_load = json.loads(line)
+            if json_load['title'] == 'イギリス':
+                return json_load['text']
+
+match = re.findall(r'\[\[(ファイル):(.+?)\|',search())
+for line in match:
+    print(line)
