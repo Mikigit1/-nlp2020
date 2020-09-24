00d00b5c313556a93599f47562a00d55b09ebcc7
MikiyaFujiki <privmikiya0812@yahoo.co.jp>
2020-09-24 23:18:50 +0900

    3−25完成

--- chart3/practice25.py
+++ chart3/practice25.py
@@ -2,18 +2,36 @@
 25. テンプレートの抽出
 記事中に含まれる「基礎情報」テンプレートのフィールド名と値を抽出し，辞書オブジェクトとして格納せよ
 '''
-#<ref>
-import json
-import re 
-import gzip
 
-def search():
-    with gzip.open('chart3/jawiki-country.json.gz',mode='rt',encoding='utf-8') as f:
-        for line in f:
-            json_load = json.loads(line)
-            if json_load['title'] == 'イギリス':
-                return json_load['text']
+import pandas as pd
+import re
 
-match = re.findall(r'\[\[(ファイル):(.+?)\|',search())
-for line in match:
-    print(line)
+df = pd.read_json("chart3/jawiki-country.json.gz", lines =  True)
+print(df)
+
+#text = df.query("title == 'イギリス'")["text"].values[0]→このままだと\nが邪魔
+text = df.query("title == 'イギリス'")["text"].values[0].split("\n")
+#print(text)で確認
+
+#正規表現で２つにグループ化する
+pattern = re.compile('\|(.+?)\s=\s(.+)')
+
+#辞書型の用意
+ans = {}
+
+for line in text:
+    result = re.search(pattern,line)
+    if result: #trueの場合
+        #print(result.group(1))→１を２に置き換えて確認
+        ans[result.group(1)] = result.group(2)
+
+print(ans)
+
+
+'''
+【参考】
+https://ohke.hateblo.jp/entry/2019/01/12/230000 #queryについて
+https://algorithm.joho.info/computer/wildcard-regular-expression/ #正規表現について
+https://python.keicode.com/lang/regular-expression-use-search.php #searchの中身確認方法
+https://note.nkmk.me/python-dict-add-update/ #辞書型の扱い
+'''
